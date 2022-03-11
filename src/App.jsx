3295dc223de2e58229216535d4689344ce1c5556
Salah Uddin <ziko.salahuddin@gmail.com>
2022-03-11 23:37:09 +0100

    Basic Structure Create

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello World by SALAH</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
+        corporis dolorum veniam incidunt nulla porro debitis ullam quae omnis
+        labore veritatis excepturi, reprehenderit ducimus odit magnam! Deserunt,
+        quisquam doloremque tenetur totam eaque odit rem iusto velit praesentium
+        mollitia eos vitae dolores molestias doloribus voluptates, animi
+        reiciendis fugit corporis rerum ipsa. Suscipit, pariatur quam obcaecati
+        non excepturi neque recusandae distinctio sint.
+      </p>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis hic
+        animi quia deleniti odio? Fugiat doloremque tempore consequuntur nihil
+        obcaecati.
+      </p>
+    </div>
+  );
+}
